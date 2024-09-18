2010c3282906542bc49c0b1f923815cd74af6c4d
Ka1juu <nerokaijuu@icloud.com>
2024-09-18 02:41:40 +0200

    worked on character.py

--- character.py
+++ character.py
@@ -1,19 +1,18 @@
 ### Das Schwarze Auge in Python
 ### using german words
 
-
 from dataclasses import dataclass
+import spezies_db
 
 @dataclass
 class Character:
-    
+    # Persönliche Daten
     name: str = ""
-    rasse: str = ""
+    spezies: str = ""
     kultur:str = ""
     profession: str = ""
-    
     xp: int = 0
-
+    # Eigenschaften
     mut: int = 0
     klugheit: int = 0
     intuition: int = 0
@@ -22,13 +21,22 @@ class Character:
     gewandtheit: int = 0
     konstitution: int = 0
     körperkraft: int = 0
-    geschwindigkeit: int = 0
 
     lebenspunkte: int = 0
-    ausdauer: int = 0
+    lp_grundwert: int = 0
     astralenergie: int = 0
     karmaenergie: int = 0
+    seelenkraft: int = 0
+    seelenkraft_grundwert: int = 0
+    zähigkeit: int = 0
+    zähigkeit_grundwert: int = 0
+    ausweichen: int = 0
+    geschwindigkeit: int = 0
+    geschwindigkeit_grundwert: int = 0
+    
+    ausdauer: int = 0
     magieresistenz: int = 0
+    
     ini_basiswert: int = 0
     at_basiswert: int = 0
     pa_basiswert: int = 0
@@ -68,8 +76,8 @@ class Character:
         current_value = getattr(self,attribute)
         setattr(self, attribute, current_value + amount)
     
-    def set_lebenspunkte(self,konstitution,körperkraft):
-        value = round(((konstitution*2) + körperkraft)/2)
+    def set_lebenspunkte(self,konstitution,le_gw):
+        value = le_gw+(2*konstitution)
         setattr(self,"lebenspunkte", value) 
     
     def set_ausdauer(self, mut, konstitution, gewandtheit):
@@ -79,6 +87,15 @@ class Character:
     def set_astralenergie(self, mut, intuition, charisma):
         value = (mut + intuition + charisma) / 2
         setattr(self, "astralenergie", value)
+
+    def set_seelenkraft(self,grundwert, mut, klugheit, intuition):
+        value = round(grundwert+((mut + klugheit + intuition) / 6))
+        setattr(self, "seelenkraft", value)
+    
+    def set_zähigkeit(self,grundwert, konstitution,körperkraft):
+        value = round(grundwert+(((konstitution*2)+körperkraft) / 6))
+        setattr(self, "zähigkeit", value)
+
 
     def set_magieresistenz(self, mut, klugheit, konstitution):
         value = (mut + klugheit + konstitution) / 5
@@ -99,16 +116,48 @@ class Character:
     def set_fk_basiswert(self, intuition, fingerfertigkeit, körperkraft):
         value = (intuition + fingerfertigkeit + körperkraft) / 5
         setattr(self, "fk_basiswert", value)
-    
 
-# Testing
+    def set_spezies(self, spezies_name):
+        spezies_data = spezies_db.spezies[spezies_name]
+        self.spezies = spezies_data["name"]
+        self.lebensenergie_grundwert = spezies_data["LP-Grundwert"]
+        self.seelenkraft_grundwert = spezies_data["Seelenkraft-Grundwert"]
+        self.zähigkeit_grundwert = spezies_data["Zähigkeit-Grundwert"]
+        for attr, value in spezies_data["Eigenschaftsänderung"].items():
+            setattr(self, attr, getattr(self, attr) + value)
+
+    def set_all_attr():
+        pass
+
+
+
+    def __str__(self):
+        return (f"Character Name: {self.name}\n"
+                f"Spezies: {self.spezies}\n"
+                f"Kultur: {self.kultur}, Profession: {self.profession}, XP: {self.xp}\n"
+                f"Mut: {self.mut}, Klugheit: {self.klugheit}, Intuition: {self.intuition}, Charisma: {self.charisma}\n"
+                f"Fingerfertigkeit: {self.fingerfertigkeit}, Gewandtheit: {self.gewandtheit}, Konstitution: {self.konstitution}, Körperkraft: {self.körperkraft}\n"
+                f"Geschwindigkeit: {self.geschwindigkeit}, LP: {self.lebenspunkte}, Seelenkraft: {self.seelenkraft}, Zähigkeit: {self.zähigkeit}\n"
+                f"Magieresistenz: {self.magieresistenz}, Astralenergie: {self.astralenergie}, Karmaenergie: {self.karmaenergie}\n"
+                f"Ini-Basiswert: {self.ini_basiswert}, AT-Basiswert: {self.at_basiswert}, PA-Basiswert: {self.pa_basiswert}, FK-Basiswert: {self.fk_basiswert}")
+
+
+# region Testing
 x = Character(name="x")
-print(x)
-
+x.set_spezies("ACHAZ")
+x.set_attr("mut",9)
+x.set_attr("klugheit",13)
+x.set_attr("intuition",10)
+x.set_attr("charisma",7)
+x.set_attr("fingerfertigkeit",10)
+x.set_attr("gewandtheit",12)
 x.set_attr("konstitution",8)
-x.set_attr("körperkraft",9)
-x.set_lebenspunkte(x.konstitution,x.körperkraft) # 8*2 = 16+9 = 25/2 = 12.5 round = 12 / round uses Bankers Round?
-print(x)
+x.set_attr("körperkraft",13)
 
-# x.equiptment_slots["Head"] = "Helmet"
-# print(x.equiptment_slots["Head"])
+x.set_attr("geschwindigkeit", x.geschwindigkeit_grundwert)
+
+x.set_lebenspunkte(x.konstitution,x.lp_grundwert) 
+x.set_seelenkraft(x.seelenkraft_grundwert,x.mut,x.klugheit,x.intuition)
+x.set_zähigkeit(x.zähigkeit_grundwert,x.konstitution,x.körperkraft)
+print(x)
+# endregion
---
+++ spezies_db.py
@@ -0,0 +1,73 @@
+spezies = {
+    "ACHAZ": 
+    {
+    "name": "Achaz",
+    "Ap-Wert": 25,
+    "LP-Grundwert": 5,
+    "Seelenkraft-Grundwert": -4,
+    "Zähigkeit-Grundwert": -5,
+    "Geschwindigkeit-Grundwert": 8,
+    "Eigenschaftsänderung": 
+    {
+        "intuition": 1,
+        "konstitution": 1,
+        "mut": -1,
+        "körperkraft": -1
+    },
+    "Übliche Kulturen": {"Achaz-Rha", "Ctki’Ssrr", "Stammesachaz"},
+    "Automatischer Vorteile": {"Dunkelsicht I", "Zusätzliche Gliedmaße (Schwanz)"},
+    "Automatischer Nachteil": {"Kältestarre": -15},
+    },
+    "ELFEN":
+    {
+    "name": "Elfen",
+    "Ap-Wert": 18,
+    "LE-Grundwert": 2,
+    "Seelenkraft-Grundwert": -4,
+    "Zähigkeit-Grundwert": -6,
+    "Geschwindigkeit-Grundwert": 8,
+    "Eigenschaftsänderung": 
+    {
+        "intuition": 1,
+        "gewandtheit": 1,
+        "klugheit": -2,
+        "körperkraft": -2
+    },
+    "Übliche Kulturen": {"Auelfen", "Firnelfen", "Waldelfen"},
+    "Automatischer Vorteile": {"Zauberer", "Zweistimmiger Gesang"},
+    },
+    "MENSCH":
+    {
+    "name": "Mensch",
+    "Ap-Wert": 0,
+    "LE-Grundwert": 5,
+    "Seelenkraft-Grundwert": -5,
+    "Zähigkeit-Grundwert": -5,
+    "Geschwindigkeit-Grundwert": 8,
+    "Eigenschaftsänderung": 
+    {
+        "Beliebige": 1,
+    },
+    "Übliche Kulturen": 
+    {
+        
+    "Mittelländer": {"Andergaster", "Bornländer", "Horasier", "Maraskaner", "Mittelreicher", "Nordaventurier", "Nostrier", "Südaventurier", "Svellttaler", "Zyklopäer"},
+    "Nivesen": {"Bornländer", "Nivesen", "Nordaventurier", "Svellttaler"},
+    "Norbarden": {"Bornländer", "Norbarden", "Nordaventurier"},
+    "Thorwaler": {"Fjarninger", "Nordaventurier", "Thorwaler"},
+    "Tulamiden": {"Aranier", "Maraskaner", "Mhanadistani", "Novadis", "Südaventurier"},
+    "Waldmenschen": {"Aranier", "Mhanadistani", "Mohas", "Novadis", "Südaventurier"},
+    "Utulus": {"Aranier", "Mhanadistani", "Mohas", "Novadis", "Südaventurier"}
+
+    },
+    },
+}
+
+
+
+# Unterschiedliche Achaz-Arten:
+# Echsensumpf-Achaz: keine Änderungen
+# Maraskan-Achaz: GE + 1 statt KO +1, Beidhändigkeit als dringend empfohlener Vorteil
+# Orkland-Achaz: FF +1 statt KO +1, keine Kältestarre bei den automatischen Vorteilen.
+# Regenwald-Achaz: KK +1 statt IN +1, IN –1 statt KK –1
+# Waldinsel-Achaz: GE +1 statt IN +1, FF –1 statt KK –1
